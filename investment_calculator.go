f488531ea48bdf849fe4ed45e4b8b4f9ed7d01f3
jon-wang-ibm <jon-wang@ibm.com>
2026-06-02 15:00:21 -0500

    up to lesson 6
    packages and main

---
+++ investment_calculator.go
@@ -0,0 +1,11 @@
+// entry point of the app
+package main
+
+import "fmt"
+
+// run `go run app.go` in terminal to execute the program
+// 'go mod init' to create modules
+// use 'go build' to create an exec file
+func main() {
+	fmt.Print("Hello, World!")
+}
