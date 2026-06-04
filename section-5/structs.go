436894cf55d3dfe15c75334fcef51d03154bcd1e
jon-wang-ibm <jon-wang@ibm.com>
2026-06-04 16:26:43 -0500

    section 5 lec 73-80
    structs, pointers, struct values as args

---
+++ section-5/structs.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser User
+
+	appUser = User{
+		userFirstName,
+		userLastName,
+		userBirthdate,
+		time.Now(),
+	}
+
+	// ... do something awesome with that gathered data!
+
+	outputUserDetails(&appUser)
+
+}
+
+func outputUserDetails(u *User) {
+	// ...
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
